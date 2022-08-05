e3e6a429e593732a2181bb3bf5ee7523933f3584
Zequi Aguirre <zequi4real@gmail.com>
2022-08-05 16:54:13 -0400

    Create onClick function to play game

--- src/memory.js
+++ src/memory.js
@@ -2,17 +2,59 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
+  // shuffleCards() {
+  //   // ... write your code here
+  //   if(!this.cards){
+  //     return undefined;
+  //   }
+    
+  //   let newArr = [];
+  //   for(let i = 0 ; i < this.cards.length; i++){
+  //     const randomIndex = Math.floor(Math.random() * this.cards.length);
+  //     // console.log(this.cards);
+  //     // console.log(newArr);
+  //     if (!newArr.includes(this.cards[randomIndex])){
+  //       // console.log(newArr.push(this.cards[randomIndex]))
+  //       newArr.push(this.cards[randomIndex])
+  //     }
+  //   }
+    
+  //   this.cards = newArr
+  
+  // }
+
   shuffleCards() {
-    // ... write your code here
+    if(!this.cards){
+        return undefined;
+      }
+    const shuffledArray = this.cards.sort((a, b) => 0.5 - Math.random());
+      this.cards = shuffledArray;
   }
+
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    this.pairsClicked++
+    if(card1 === card2) {
+      this.pairsGuessed++;
+      return true
+    } else {
+      return false
+    }
   }
 
   checkIfFinished() {
     // ... write your code here
+    if (this.pairsGuessed == 12) {
+      return true;
+    } else {
+      return false
+    }
+  
   }
 }
